ca77aed051ded05d17c5ef7687c6e7e81a6d2ee8
Sofiane Mallem <smallem@3-F-14.42heilbronn.de>
2023-09-25 20:13:15 +0200

    h

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: smallem <smallem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 20:56:41 by smallem           #+#    #+#             */
-/*   Updated: 2023/09/25 17:32:22 by smallem          ###   ########.fr       */
+/*   Updated: 2023/09/25 19:00:03 by smallem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,34 @@ char	**copy_env(char **env, t_term *term)
 		new_ev[len] = ft_strdup(env[len], term);
 	new_ev[len] = NULL;
 	return (new_ev);
+}
+
+char	*get_path(t_term *term, char *cmd)
+{
+	int	i;
+	char	*path;
+	char	*line;
+	char	**ppath;
+
+	i = -1;
+	path = NULL;
+	while (term->env[++i])
+	{
+		if (!ft_strncmp(term->env[i], "PATH=", ft_strlen("PATH=")))
+		{
+			path = ft_strdup(term->env[i], term);
+			break ;
+		}
+	}
+	if (!path)
+		return (NULL);
+	ppath = ft_split(path, ':', term);
+	i = -1;
+	while (ppath[++i])
+	{
+		line = ft_strjoin(ft_strjoin(ppath[i], "/", term), cmd, term);
+		if (access(X_OK | F_OK, line) == 0)
+			return (line);
+	}
+	return (NULL);
 }
